eddcea9e465a4d52df1d34e82e1dbf0cb590d962
Aman Garg <aman.ambah@gmail.com>
2024-04-29 10:38:20 +0530

    Raise error for non-string input

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,5 +1,6 @@
 module Calculator
 	def add(string)
+		raise ArgumentError, 'Input must be a string' if !string.is_a?(String)
 		return 0 if string.empty?
 	end
 end
--- spec/string_calc_spec.rb
+++ spec/string_calc_spec.rb
@@ -2,6 +2,10 @@ require_relative '../main'
 RSpec.describe StringCalc do
 	describe "#add function" do
 		subject(:input_value) { described_class.new }
+
+		it "return error if input is not a string" do
+			expect { input_value.add(1) }.to raise_error(ArgumentError, 'Input must be a string')
+		end
 		
 		it "return 0 for empty string" do
 			expect(input_value.add('')).to eq(0)
