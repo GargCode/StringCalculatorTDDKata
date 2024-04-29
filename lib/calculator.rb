c40872e5050d19c74d53a40efdc215f79fff4fdc
Aman Garg <aman.ambah@gmail.com>
2024-04-29 11:28:44 +0530

    test for Support different delimiters

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,21 +1,34 @@
 module Calculator
-	def add(string)
-		raise ArgumentError, 'Input must be a string' if !string.is_a?(String)
-		return 0 if string.empty?
-		numbers = split_string(string)
-		validate_array(numbers)
+  def add(string)
+    raise ArgumentError, 'Input must be a string' if !string.is_a?(String)
+    return 0 if string.empty?
+    numbers = extract_numbers(string)
+    validate_array(numbers)
 
-		numbers.map(&:to_i).sum
-	end
+    numbers.map(&:to_i).sum
+  end
 
-	private
+  private
 
-	def split_string(string)
-		numbers = string.delete(' ')
-		numbers&.split(/#{Regexp.escape(',')}|,|\n/)
-	end
+  def extract_numbers(string)
+      string = string.gsub('\\n', "\n")
+      if string.start_with?('//')
+        delimiter_declaration, numbers = string.split("\n", 2)
+        delimiter = delimiter_declaration[2..]
+      else
+        delimiter = nil
+        numbers = string
+      end
 
-	def validate_array(numbers)
-		raise ArgumentError, 'Invalid Input' if numbers&.any?(&:empty?)
-	end
+      split_string(numbers, delimiter || ',')
+    end
+
+  def split_string(string, delimiter)
+    numbers = string.delete(' ')
+    numbers&.split(/#{Regexp.escape(delimiter)}|,|\n/)
+  end
+
+  def validate_array(numbers)
+    raise ArgumentError, 'Invalid Input' if numbers&.any?(&:empty?)
+  end
 end
--- spec/string_calc_spec.rb
+++ spec/string_calc_spec.rb
@@ -1,34 +1,40 @@
 require_relative '../main'
 RSpec.describe StringCalc do
-	describe "#add function" do
-		subject(:input_value) { described_class.new }
+  describe "#add function" do
+    subject(:input_value) { described_class.new }
 
-		it "return error if input is not a string" do
-			expect { input_value.add(1) }.to raise_error(ArgumentError, 'Input must be a string')
-		end
-		
-		it "return 0 for empty string" do
-			expect(input_value.add('')).to eq(0)
-		end
+    it "return error if input is not a string" do
+      expect { input_value.add(1) }.to raise_error(ArgumentError, 'Input must be a string')
+    end
+    
+    it "return 0 for empty string" do
+      expect(input_value.add('')).to eq(0)
+    end
 
-		it "return the same number if has single input" do
-			expect(input_value.add('1')).to eq(1)
-		end
+    it "return the same number if has single input" do
+      expect(input_value.add('1')).to eq(1)
+    end
 
-		it "add number seprate by ','" do
-			expect(input_value.add('1, 2, 3')).to eq(6)
-		end
+    it "add number seprate by ','" do
+      expect(input_value.add('1, 2, 3')).to eq(6)
+    end
 
-		it 'handles newlines as delimiters' do
-	      expect(input_value.add("1\n2	,3")).to eq(6)
-	    end
+    it 'handles newlines as delimiters' do
+        expect(input_value.add("1\n2  ,3")).to eq(6)
+      end
 
-	    it 'handles newlines as delimiters' do
-	      expect(input_value.add("1\n2\n3")).to eq(6)
-	    end
+      it 'handles newlines as delimiters' do
+        expect(input_value.add("1\n2\n3")).to eq(6)
+      end
 
-	    it 'raises ArgumentError for invalid input with a comma followed by a newline' do
-	      expect { input_value.add("1,\n2") }.to raise_error(ArgumentError)
-	    end
-	end
+      it 'raises ArgumentError for invalid input with a comma followed by a newline' do
+        expect { input_value.add("1,\n2") }.to raise_error(ArgumentError)
+      end
+
+      it 'changes of delimiter at the beginning of the string' do
+        expect(input_value.add("//;\n1;2")).to eq(3)
+        expect(input_value.add("//*\n1\n2*3\n4")).to eq(10)
+        expect(input_value.add("//#\n1\n2#5")).to eq(8)
+      end
+  end
 end
